f28187ef3e27c84f4486e534ef53fc0989f8baec
Zachary Larson <Zach@Zacharys-MacBook-Pro.local>
2021-07-21 12:04:40 -0400

    update to site with firestore db

--- scripts.js
+++ scripts.js
@@ -2,14 +2,46 @@ function handleSubmit(event) {
     event.preventDefault();
     console.log("Submitted Form");
     const form = document.querySelector("form");
-    form.innerHTML = "<h3>Thanks</h3>";
+    const newCustomer = {
+      firstName: form.elements.firstName.value,
+      lastName: form.elements.lastName.value,
+      email: form.elements.email.value
+    }
+    console.log(newCustomer)
+    fetch('https://zl-pet-boutique.web.app/customers',{
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify(newCustomer)
+    })
+    
+      .then(() => {
+        form.innerHTML = "<h3>Thanks</h3>"
+        getCustomers()
+      })
+      .catch(err => {
+        form.innerHTML = '<h3>Error sending customer</h3>'
+      })
   }
   function handleNameChange() {
     console.log("Name Changed", event.target.value);
   }
-  const hero = document.getElementById("hero");
-  hero.innerText = "This is my new Hero Title";
-  const links = document.getElementsByClassName("menu-link");
-  links[1].style.color = "white";
-  const footer = document.querySelector("footer");
-  footer.style.backgroundColor = "grey";
+  function getCustomers(){
+  fetch('https://zl-pet-boutique.web.app/customers')
+    .then(response => response.json())
+    .then(data => {
+      const custDiv = document.getElementById('customers')
+      custDiv.innerHTML = ''
+      const custArray = data.map(customer => {
+        return `<article>
+        <h3>${customer.firstName} ${customer.lastName}</h3>
+        <p>${customer.email}</p>
+        </artcile>`
+      })
+      custDiv.innerHTML = custArray.join('')
+    })
+    .catch(err => console.error(err))
+  }
+
+  getCustomers()
